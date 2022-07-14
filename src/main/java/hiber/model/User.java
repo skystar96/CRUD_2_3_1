60456e672ae94bca8dd4836850a78916ad6a3989
skystar96 <skystar_uz@mail.ru>
2022-07-14 12:10:22 +0300

    C5

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -1,6 +1,7 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -70,12 +71,18 @@ public class User {
    }
 
    @Override
-   public int hashCode() {
-      return super.hashCode();
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      User user = (User) o;
+      return Objects.equals(id, user.id)
+              && Objects.equals(firstName, user.firstName)
+              && Objects.equals(lastName, user.lastName)
+              && Objects.equals(email, user.email);
    }
 
    @Override
-   public boolean equals(Object obj) {
-      return super.equals(obj);
+   public int hashCode() {
+      return Objects.hash(id, firstName, lastName, email);
    }
 }
